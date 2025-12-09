30902676eb29c495c8cb60910886b60aea1ae541
Pankaj Kumar Mishra <pankajkumarmishra808@gmail.com>
2025-12-10 01:22:53 +0530

    .

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './components/card'
+const App = () => {
+  return (
+    <div className='parent'>
+   
+     <Card/>
+     </div>
+  )
+}
+
+export default App
---
+++ 03-props/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+   
+    <div>
+      <div className='card'>
+         <img src="https://images.unsplash.com/photo-1763987209766-d9bb8836c0e2?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        <h1>my Name is Pankaj</h1>
+      <p>vuhdgeuyfhqeb y8yeyeenq qe f  78y9f78y8  ryr ryryr ryry</p>
+      <button>View Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
